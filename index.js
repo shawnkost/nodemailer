038c9d519c9f68dde29740df7e1a8eaaa6c247da
Shawn Kost <shawn.kost@twisted-rope.com>
2022-01-27 22:03:49 -0800

    Implement basic functionality to send emails

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const nodemailer = require('nodemailer');
+require('dotenv/config');
 
 const transporter = nodemailer.createTransport({
   service: 'hotmail',
   auth: {
-    user: '',
-    pass: '',
+    user: process.env.USER,
+    pass: process.env.PASS,
   },
+});
+
+const options = {
+  from: process.env.USER,
+  to: process.env.RECEIVER,
+  subject: 'Sending email with node.js!',
+  text: "wow! That's simple!",
+};
+
+transporter.sendMail(options, function (err, info) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log('Sent:' + info.response);
 });
